70769617c41c468217197abf0f260d2733797cd9
Gugya <71634961+Leves01@users.noreply.github.com>
2021-11-22 13:20:05 +0100

    Users absztrakt class alapjai
    Generátor nincs benne még, + a konstruktorban nem vagyok biztos

---
+++ users.h
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Users {
+    private:
+        string fullName;
+        string dateOfBirth;
+        string address;
+        string password;
+        string codeOfUser;
+
+    public:
+    Users();
+    // Users(string fullName, string dateOfBirth, string address, string password, string codeOfUser);
+    
+    // '4 karakterből álló generátor
+    // +userCodeGenerator() : string
+    
+    string getFullName();
+    string getDateOfBirth();
+    string getAddress();
+    void setFullName(string newFullName);
+    void setDateOfBirth(string newDateOfBirth);
+    void setAddress(string newAddress);
+    void setPassword(string newPassword);
+};
