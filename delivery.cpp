576be477074f943278dfeb1e0442dffccfc1b839
donatboros <94782590+donatboros@users.noreply.github.com>
2021-12-05 14:00:47 +0100

    Add files via upload

---
+++ delivery.cpp
@@ -0,0 +1,31 @@
+#include "Delivery.h"
+
+Delivery::Delivery()
+{
+}
+
+std::string Delivery::generateDeliveryCode()
+{
+	return std::string();
+}
+
+std::string Delivery::getDeliveryAddress()
+{
+	return std::string();
+}
+
+std::string Delivery::getDeliveryCode()
+{
+	return std::string();
+}
+
+std::string Delivery::listDeliveriesByCode(std::string deliveryCode)
+{
+	return std::string();
+}
+
+std::string Delivery::listDeliveriesByCountry(std::string deliveryAddress)
+{
+	return std::string();
+}
+
